fa2dca86cc19d95f355baf559c5f0ff7b896d2b5
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-21 04:42:32 +0300

    Updated FindHackDTO

--- Api/DTOs/FindHackDTO.cs
+++ Api/DTOs/FindHackDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace Api.DTOs
+﻿using System.ComponentModel;
+
+namespace Api.DTOs
 {
     public class FindHackDTO
     {
-        public int contest { get; set; }
         public string problem { get; set; }
-        public string test { get; set; }
+        public string input { get; set; }
+        [DefaultValue(null)]
+        public string? output { get; set; } = null;
     }
 }
