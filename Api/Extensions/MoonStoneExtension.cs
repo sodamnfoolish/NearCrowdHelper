bd337f73693ec3d88e130c38b87fe447380bc60c
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-21 04:38:08 +0300

    Created MoonStoneExtension

---
+++ Api/Extensions/MoonStoneExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Extensions
+{
+    public static class MoonStoneExtension
+    {
+        public static int GetContestId(this string problem)
+            => Convert.ToInt32(problem.Substring(0, problem.Length - 1));
+
+        public static string GetIndex(this string problem)
+            => problem.Substring(problem.Length - 1, 1);
+    }
+}
