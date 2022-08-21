dd7861e0d7a47eae270e9a96d8240c91daeb753b
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-21 04:42:50 +0300

    Updated MoonStoneService

--- Api/Services/MoonStoneService.cs
+++ Api/Services/MoonStoneService.cs
@@ -1,10 +1,17 @@
 ﻿using Api.DTOs;
+using Api.Extensions;
 
 namespace Api.Services
 {
     public class MoonStoneService
     {
         private readonly HttpClient _cfApiHttpClient;
+
+        private const string _inputPrefix = "Input:\n";
+        private const string _inputSuffix = "\r\n\n\nOutput";
+
+        private const string _outputPrefix = "";
+        private const string _outputSuffix = "";
 
         public MoonStoneService(IConfiguration configuration)
         {
@@ -13,17 +20,24 @@ namespace Api.Services
             _cfApiHttpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
         }
 
-        public async Task<IEnumerable<string>> FindHack(int contest, string problem, string test) 
+        public async Task<IEnumerable<string>> FindHack(string problem, string input, string? output = null) 
         {
+            var contestId = problem.GetContestId();
+            var index = problem.GetIndex();
+
             try
             {
-                HttpResponseMessage httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.hacks?contestId={contest}");
+                HttpResponseMessage httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.hacks?contestId={contestId}");
 
                 var hacks = (await httpResponseMessage.Content.ReadFromJsonAsync<ContestHacksDTO>()).result;
 
                 return hacks
-                    .Where(h => h.judgeProtocol.protocol.Contains(test) && h.problem.index == problem)
-                    .Select(h => h.defender.members.FirstOrDefault().handle);
+                    .Where(h => 
+                        h.judgeProtocol.protocol.Contains(_inputPrefix + input + _inputSuffix) && 
+                        (output == null || h.judgeProtocol.protocol.Contains(_outputPrefix + output + _outputSuffix)) && 
+                        h.problem.index == index)
+                    .Select(h => 
+                        h.defender.members.FirstOrDefault().handle);
             }
             catch (Exception e)
             {
