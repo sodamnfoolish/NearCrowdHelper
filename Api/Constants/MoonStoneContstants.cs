71873678df0ca2f801202e1de8b828cb9ea1ba4f
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-22 20:47:41 +0300

    Created MoonStoneConstants

---
+++ Api/Constants/MoonStoneContstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Api.Contstants
+{
+    public class MoonStoneConstants
+    {
+        public const string InputPrefix = "Input:\n";
+        public const string InputSuffix = "\r\n\n\nOutput";
+
+        public const string OutputPrefix = "Output:\n";
+        public const string OutputSuffix = "\r\n\n\nAnswer:";
+
+        public const string ProgrammingLanguagePrefix = "GNU C++";
+
+        public enum Verticts
+        {
+            OK = 0
+        };
+
+        public enum ParticipantTypes
+        {
+            CONTESTANT = 0
+        };
+    }
+}
