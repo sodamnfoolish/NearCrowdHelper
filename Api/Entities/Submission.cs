d1211e41bcb0eecbbb19436cec833fe3f8643ea1
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-22 19:19:14 +0300

    Created Submission entity

---
+++ Api/Entities/Submission.cs
@@ -0,0 +1,18 @@
+﻿namespace Api.Entities
+{
+    public class Submission
+    {
+        public int id { get; set; }
+        public int? contestId { get; set; }
+        public int creationTimeSeconds { get; set; }
+        public int relativeTimeSeconds { get; set; }
+        public Problem problem { get; set; }
+        public Party author { get; set; }
+        public string programmingLanguage { get; set; }
+        public string? verdict { get; set; }
+        public string testset { get; set; }
+        public int passedTestCount { get; set; }
+        public int timeConsumedMillis { get; set; }
+        public float? memoryConsumedBytes { get; set; }
+    }
+}
