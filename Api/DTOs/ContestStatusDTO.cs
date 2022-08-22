a217b48bcf7af7156f9993cd3e8979069cadaa8e
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-22 19:19:43 +0300

    Created ContestStatusDTO

---
+++ Api/DTOs/ContestStatusDTO.cs
@@ -0,0 +1,10 @@
+﻿using Api.Entities;
+
+namespace Api.DTOs
+{
+    public class ContestStatusDTO
+    {
+        public string status { get; set; }
+        public List<Submission> result { get; set; }
+    }
+}
