431cb51b088525996925eb92a9664de29d178006
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-22 20:47:51 +0300

    Updated MoonStoneService

--- Api/Services/MoonStoneService.cs
+++ Api/Services/MoonStoneService.cs
@@ -1,25 +1,12 @@
-﻿using Api.Extensions;
-using Api.Entities;
+﻿using Api.Entities;
 using Api.DTOs;
+using Api.Contstants;
 
 namespace Api.Services
 {
     public class MoonStoneService
     {
         private readonly HttpClient _cfApiHttpClient;
-
-        private const string _inputPrefix = "Input:\n";
-        private const string _inputSuffix = "\r\n\n\nOutput";
-
-        private const string _outputPrefix = "";
-        private const string _outputSuffix = "";
-
-        private enum _verticts
-        {
-            OK = 0
-        };
-
-        private const string _programmingLanguagePrefix = "GNU C++";
 
         public MoonStoneService(IConfiguration configuration)
         {
@@ -30,55 +17,34 @@ namespace Api.Services
 
         public async Task<IEnumerable<string>> FindHandlesByHack(int contestId, string index, string input, string? output = null) 
         {
-            var hacks = new List<Hack>();
+            var httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.hacks?contestId={contestId}");
 
-            try
-            {
-                HttpResponseMessage httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.hacks?contestId={contestId}");
-
-                hacks = (await httpResponseMessage.Content.ReadFromJsonAsync<CFApiResponseDTO<Hack>>()).result;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-
-                throw new Exception("There are some problems with CF API");
-            }
+            var hacks = (await httpResponseMessage.Content.ReadFromJsonAsync<CFApiResponseDTO<Hack>>())!.result;
 
             return hacks
                 .Where(h =>
-                    h.judgeProtocol.protocol.Contains(_inputPrefix + input + _inputSuffix) &&
-                    (output == null || h.judgeProtocol.protocol.Contains(_outputPrefix + output + _outputSuffix)) &&
+                    h.judgeProtocol.protocol.Contains(MoonStoneConstants.InputPrefix + input + MoonStoneConstants.InputSuffix) &&
+                    (output == null || h.judgeProtocol.protocol.Contains(MoonStoneConstants.OutputPrefix + output + MoonStoneConstants.OutputSuffix)) &&
                     h.problem.index == index)
                 .Select(h =>
-                    h.defender.members.FirstOrDefault().handle);
+                    h.defender.members.FirstOrDefault()!.handle);
         }
 
         public async Task<IEnumerable<int>> FindOkSubmissionsByHack(int contestId, string index, string input, string? output = null)
         {
             var handles = (await FindHandlesByHack(contestId, index, input, output)).ToList();
 
-            var submissions = new List<Submission>();
+            var httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.status?contestId={contestId}");
 
-            try
-            {
-                HttpResponseMessage httpResponseMessage = await _cfApiHttpClient.GetAsync($"contest.status?contestId={contestId}");
-
-                submissions = (await httpResponseMessage.Content.ReadFromJsonAsync<CFApiResponseDTO<Submission>>()).result;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-
-                throw new Exception("There are some problems with CF API");
-            }
+            var submissions = (await httpResponseMessage.Content.ReadFromJsonAsync<CFApiResponseDTO<Submission>>())!.result;
 
             return submissions
                 .Where(s =>
-                    handles.Contains(s.author.members.FirstOrDefault().handle) &&
+                    handles.Contains(s.author.members.FirstOrDefault()!.handle) &&
                     s.problem.index == index &&
-                    s.verdict == _verticts.OK.ToString() &&
-                    s.programmingLanguage.Contains(_programmingLanguagePrefix))
+                    s.verdict == MoonStoneConstants.Verticts.OK.ToString() &&
+                    s.author.participantType == MoonStoneConstants.ParticipantTypes.CONTESTANT.ToString() &&
+                    s.programmingLanguage.Contains(MoonStoneConstants.ProgrammingLanguagePrefix))
                 .Select(s => s.id);
         }
     }
