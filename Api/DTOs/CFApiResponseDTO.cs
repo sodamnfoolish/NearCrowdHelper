9a31086995f56ff59999fbaa34a13a5f7186f6e0
sodamnfoolish <n.zhyikov@gmail.com>
2022-08-22 19:19:33 +0300

    Created CFApiResponseDTO

---
+++ Api/DTOs/CFApiResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.DTOs
+{
+    public class CFApiResponseDTO<T> where T : class
+    {
+        public string status { get; set; }
+        public List<T> result { get; set; }
+    }
+}
